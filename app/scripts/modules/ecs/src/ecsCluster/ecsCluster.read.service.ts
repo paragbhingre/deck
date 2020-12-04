09b4b52f7cbdbd266c0f326a576a39939aaef75d
Parag Bhingre <pbhingre@gmail.com>
2020-12-04 11:24:43 -0800

    Get describe cluster details

--- app/scripts/modules/ecs/src/ecsCluster/ecsCluster.read.service.ts
+++ app/scripts/modules/ecs/src/ecsCluster/ecsCluster.read.service.ts
@@ -2,11 +2,17 @@ import { module } from 'angular';
 
 import { API } from '@spinnaker/core';
 import { IEcsClusterDescriptor } from './IEcsCluster';
+import {IEcsDescribeCluster} from "ecs/ecsCluster/IEcsDescribeCluster";
 
 export class EcsClusterReader {
   public listClusters(): PromiseLike<IEcsClusterDescriptor[]> {
     return API.all('ecs').all('ecsClusters').getList();
   }
+
+  public listDescribeClusters(account: string, region: string): PromiseLike<IEcsDescribeCluster[]> {
+    return API.all('ecs').all('ecsDescribeClusters').all(account).all(region).getList();
+  }
+
 }
 
 export const ECS_CLUSTER_READ_SERVICE = 'spinnaker.ecs.ecsCluster.read.service';
--- app/scripts/modules/ecs/src/serverGroup/configure/serverGroupConfiguration.service.ts
+++ app/scripts/modules/ecs/src/serverGroup/configure/serverGroupConfiguration.service.ts
@@ -38,6 +38,7 @@ import { IMetricAlarmDescriptor } from '../../metricAlarm/MetricAlarm';
 import { PlacementStrategyService } from '../../placementStrategy/placementStrategy.service';
 import { IPlacementStrategy } from '../../placementStrategy/IPlacementStrategy';
 import { IEcsClusterDescriptor } from '../../ecsCluster/IEcsCluster';
+import { IEcsDescribeCluster } from '../../ecsCluster/IEcsDescribeCluster'
 import { SecretReader } from '../../secrets/secret.read.service';
 import { ISecretDescriptor } from '../../secrets/ISecret';
 import { ServiceDiscoveryReader } from '../../serviceDiscovery/serviceDiscovery.read.service';
@@ -76,12 +77,14 @@ export interface IEcsServerGroupCommandBackingDataFiltered extends IServerGroupC
   secrets: string[];
   serviceDiscoveryRegistries: IServiceDiscoveryRegistryDescriptor[];
   images: IEcsDockerImage[];
+  ecsDescribeCluster: IEcsDescribeCluster[];
 }
 
 export interface IEcsServerGroupCommandBackingData extends IServerGroupCommandBackingData {
   filtered: IEcsServerGroupCommandBackingDataFiltered;
   targetGroups: string[];
   ecsClusters: IEcsClusterDescriptor[];
+  ecsDescribeCluster: IEcsDescribeCluster[];
   iamRoles: IRoleDescriptor[];
   metricAlarms: IMetricAlarmDescriptor[];
   launchTypes: string[];
@@ -136,6 +139,7 @@ export interface IEcsServerGroupCommand extends IServerGroupCommand {
   serviceDiscoveryAssociations: IEcsServiceDiscoveryRegistryAssociation[];
   useTaskDefinitionArtifact: boolean;
 
+  ecsDescribeCluster: IEcsDescribeCluster[];
   subnetTypeChanged: (command: IEcsServerGroupCommand) => IServerGroupCommandResult;
   placementStrategyNameChanged: (command: IEcsServerGroupCommand) => IServerGroupCommandResult;
   regionIsDeprecated: (command: IEcsServerGroupCommand) => boolean;
@@ -241,6 +245,7 @@ export class EcsServerGroupConfigurationService {
         subnets: SubnetReader.listSubnetsByProvider('ecs'),
         iamRoles: this.iamRoleReader.listRoles('ecs'),
         ecsClusters: this.ecsClusterReader.listClusters(),
+        ecsDescribeCluster: this.ecsClusterReader.listDescribeClusters(cmd.credentials, cmd.region),
         metricAlarms: this.metricAlarmReader.listMetricAlarms(),
         securityGroups: this.securityGroupReader.getAllSecurityGroups(),
         launchTypes: this.$q.when(clone(this.launchTypes)),
@@ -262,6 +267,7 @@ export class EcsServerGroupConfigurationService {
         this.configureAvailableSubnetTypes(cmd);
         this.configureAvailableSecurityGroups(cmd);
         this.configureAvailableEcsClusters(cmd);
+        this.configureAvailableEcsDescribeClusters(cmd);
         this.configureAvailableSecrets(cmd);
         this.configureAvailableServiceDiscoveryRegistries(cmd);
         this.configureAvailableImages(cmd);
@@ -271,6 +277,37 @@ export class EcsServerGroupConfigurationService {
         this.attachEventHandlers(cmd);
       });
   }
+
+  public configureAvailableEcsDescribeClusters(command: IEcsServerGroupCommand): void {
+    command.backingData.filtered.ecsDescribeCluster = chain(command.backingData.ecsDescribeCluster)
+      .filter({
+        account: command.credentials,
+        region: command.region,
+      })
+      .map((cluster) => this.mapDescribeClusters(cluster))
+      .value();
+  }
+
+  public mapDescribeClusters(describeClusters: IEcsDescribeCluster) : IEcsDescribeCluster {
+    return {
+      activeServicesCount : describeClusters.activeServicesCount,
+      attachments : describeClusters.attachments,
+      attachmentsStatus : describeClusters.attachmentsStatus,
+      capacityProviders : describeClusters.capacityProviders,
+      clusterArn : describeClusters.clusterArn,
+      clusterName :describeClusters.clusterName,
+      defaultCapacityProviderStrategy : describeClusters.defaultCapacityProviderStrategy,
+      pendingTasksCount : describeClusters.pendingTasksCount,
+      registeredContainerInstancesCount : describeClusters.registeredContainerInstancesCount,
+      runningTasksCount : describeClusters.runningTasksCount,
+      settings : describeClusters.settings,
+      statistics : describeClusters.statistics,
+      status : describeClusters.status,
+      tags : describeClusters.tags,
+      failures: describeClusters.failures
+    };
+  }
+
 
   public applyOverrides(phase: string, command: IEcsServerGroupCommand): void {
     this.serverGroupCommandRegistry.getCommandOverrides('ecs').forEach((override: any) => {
