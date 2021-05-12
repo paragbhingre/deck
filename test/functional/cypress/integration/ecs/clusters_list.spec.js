3533eba392d213b8b675547861fb4dcfd562fd17
Parag Bhingre <pbhingre@amazon.com>
2021-05-12 10:34:56 -0400

    Instance Details not found

--- test/functional/cypress/integration/ecs/clusters_list.spec.js
+++ test/functional/cypress/integration/ecs/clusters_list.spec.js
@@ -9,6 +9,8 @@ describe('Amazon ECS: aws-prod-ecsdemo cluster', () => {
       '/applications/ecsapp/serverGroups/**/aws-prod-ecsdemo-v000?includeDetails=false',
       'fixture:ecs/clusters/serverGroup.ecsdemo-v000.json',
     );
+    cy.route('/instances/ecs-my-aws-devel-acct/us-west-2/f8757e00-184d-4288-b535-4124a739e7be',
+      'fixture:ecs/clusters/serverGroup.ecsdemo-InstanceDetails_v000.json');
   });
 
   it('shows stored ECS cluster with their sequences', () => {
@@ -50,5 +52,40 @@ describe('Amazon ECS: aws-prod-ecsdemo cluster', () => {
       .click()
       .get('a:contains("Rollback")')
       .click();
+  });
+
+  it('shows stored details view and ECS server group actions1', () => {
+    cy.visit('#/applications/ecsapp/clusters');
+
+    cy.get('.sub-group:contains("aws-prod-ecsdemo")')
+      .find('.server-group:contains("v000")')
+      .find('.cluster-container')
+      .find('.instances')
+      .find('.instance-group')
+      .get('a[title="f8757e00-184d-4288-b535-4124a739e7be"]')
+      .click();
+
+
+    cy.get('.details-panel > .header')
+      .get('instance-details-header')
+      .get('.header-text')
+      .get('h3:contains("5880e2f5d22e485d834d1d38861147aa")');
+
+    cy.get('.details-panel > .content > .collapsible-section').eq(0)
+      .get('.content-body > dl > dd');
+
+    cy.get('.details-panel > .content > .collapsible-section').eq(1)
+      .get('.content-body > dl > dd').eq(0)
+      .get('span:contains("Up")');
+
+    cy.get('.details-panel > .content > .collapsible-section').eq(1)
+      .get('.content-body > dl > dd').eq(1)
+      .get('span:contains("Up")');
+
+    cy.get('.details-panel > .content > .collapsible-section > .section-heading > .collapsible-heading').eq(2)
+      .click();
+
+    cy.get('.details-panel > .content > .collapsible-section > .content-body > dl > dd').eq(0)
+      .get('vpc-tag > span:contains("SpinnakerVPC (vpc-0a9ccfd1cba7bc715)")');
   });
 });
